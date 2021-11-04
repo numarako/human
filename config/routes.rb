60624f91acdfd2fa51677656809b4ed4d62a802f
numarako <s12257010@gmail.com>
2021-11-04 15:46:26 +0900

    user regisration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  root 'static_pages#home'
+  root   'static_pages#home'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  get  '/signup',  to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_084827) do
+ActiveRecord::Schema.define(version: 2021_11_03_095230) do
 
   create_table "content_favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2021_11_02_084827) do
     t.integer "gender"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
