32c50c76dfa627695b595c4ab4d3e494f08b845a
numarako <s12257010@gmail.com>
2021-11-25 14:25:25 +0900

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,33 +7,50 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(name: "1", email: "user1@example.com", age: "under15", gender: "man", password:"password", password_confirmation: "password")
 User.create!(name: "nao", email: "user2@example.com", age: "under15", gender: "man", password:"password", password_confirmation: "password")
-#Content.create(title: "失敗", emotions: 1, score: 80, journaling: "ひどい" 
-#  , situation: "上司に怒られた", compassion: "次頑張ろう！", status: 0)
 
 # contents作成
 user = User.find_by(name: "1")
+user2 = User.find_by(name: "nao")
 emotions = 1
 score = 80
 status = 1
 
 20.times do
-  title = Faker::Lorem.sentence(word_count: 5)
-  journaling = Faker::Lorem.sentence(word_count: 5)
-  situation = Faker::Lorem.sentence(word_count: 5)
-  compassion = Faker::Lorem.sentence(word_count: 5)
+  title = Faker::Lorem.sentence(word_count: 10)
+  journaling = Faker::Lorem.sentence(word_count: 100)
+  situation = Faker::Lorem.sentence(word_count: 100)
+  compassion = Faker::Lorem.sentence(word_count: 100)
   user.contents.create!(title: title, emotions: emotions, score: score, journaling: journaling, situation: situation, compassion: compassion, status: status)
+  user2.contents.create!(title: title, emotions: emotions, score: score, journaling: journaling, situation: situation, compassion: compassion, status: status)
 end
 
 # イベント作成
-user2 = User.find_by(name: "nao")
+Event.create(user_id: user.id, mind: 1, reason: "test", small_success: "test", small_thanks: "test", date: "2021-11-23")
+Event.create(user_id: user.id, mind: 2, reason: "test", small_success: "test", small_thanks: "test",date: "2021-11-19")
+Event.create(user_id: user.id, mind: 3, reason: "test", small_success: "test", small_thanks: "test",date: "2021-11-20")
+Event.create(user_id: user.id, mind: 4, reason: "test", small_success: "test", small_thanks: "test",date: "2021-11-21")
+Event.create(user_id: user.id, mind: 0, reason: "test", small_success: "test", small_thanks: "test",date: "2021-11-22")
 Event.create(user_id: user2.id, mind: 1, reason: "test", small_success: "test", small_thanks: "test", date: "2021-11-23")
 Event.create(user_id: user2.id, mind: 2, reason: "test", small_success: "test", small_thanks: "test",date: "2021-11-19")
 Event.create(user_id: user2.id, mind: 3, reason: "test", small_success: "test", small_thanks: "test",date: "2021-11-20")
 Event.create(user_id: user2.id, mind: 4, reason: "test", small_success: "test", small_thanks: "test",date: "2021-11-21")
 Event.create(user_id: user2.id, mind: 0, reason: "test", small_success: "test", small_thanks: "test",date: "2021-11-22")
 
+# お気に入り作成
+10.times do
+  content_id = rand(1..40)
+  if content_id%2 == 0
+    user.content_bookmarks.create(content_id: content_id)
+  else
+    user2.content_bookmarks.create(content_id: content_id)
+  end
+end
+
 # 自己メモ追加
-Memo.create(user_id: user.id, category: 0, title: "test", text: "test")
-Memo.create(user_id: user.id, category: 1, title: "test", text: "test")
-Memo.create(user_id: user.id, category: 2, title: "test", text: "test")
-Memo.create(user_id: user.id, category: 3, title: "test", text: "test")
+20.times do
+  category = rand(0..3)
+  title = Faker::Lorem.sentence(word_count: 10)
+  text = Faker::Lorem.sentence(word_count: 100)
+  user.memos.create!(title: title, text: text, category: category)
+  user2.memos.create!(title: title, text: text, category: category)
+end
