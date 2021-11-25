ab477e945afe8fd413ec13f8d9451b446bdd849c
numarako <s12257010@gmail.com>
2021-11-25 15:42:53 +0900

    about this site

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'events/index'
   root   'contents#index'
+  get    '/about',   to: 'static_pages#about'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
